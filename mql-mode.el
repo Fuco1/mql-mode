1891c430cd511a95390141d809481134c41508d2
Matus Goljer <matus.goljer@gmail.com>
2016-06-02 21:53:26 +0200

    Add constants font-lock

--- mql-mode.el
+++ mql-mode.el
@@ -34,6 +34,8 @@
 (defvar mql-mode-keywords
   `(("\\<\\(?:Ask\\|B\\(?:ars\\|id\\)\\|Close\\|Digits\\|High\\|Low\\|Open\\|Point\\|\\(?:Ti\\|Volu\\)me\\)\\>"
      . font-lock-builtin-face)
+    (,(regexp-opt '("MODE_STOPLEVEL" "SELECT_BY_TICKET" "INIT_SUCCEEDED" "OP_BUY" "OP_SELL"))
+     . font-lock-constant-face)
     (,(concat "\\<\\(?:A\\(?:ccount\\(?:Balance\\|C\\(?:ompany\\|redit\\|ur"
               "rency\\)\\|Equity\\|FreeMargin\\(?:Check\\|Mode\\)?\\|Levera"
               "ge\\|Margin\\|N\\(?:ame\\|umber\\)\\|Profit\\|S\\(?:erver\\|"
